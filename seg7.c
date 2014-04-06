211ddf78d4275b6110c36dcf618d284a60d229d5
András Veres-Szentkirályi <vsza@vsza.hu>
2014-04-06 15:24:35 +0200

    calculate DIGIT_MASK from NUM_DIGITS and DIGIT_OFFSET

--- seg7.c
+++ seg7.c
@@ -17,9 +17,11 @@ volatile uint8_t framebuf[NUM_DIGITS];
 #define TIMER1_PRESCALE_1024 5
 
 uint8_t cur_digit = 0;
+uint8_t DIGIT_MASK = 0xFF;
+
 ISR( TIMER1_COMPA_vect ) {
 	if (cur_digit++ >= NUM_DIGITS) cur_digit = 0;
-	PORTD &= 255 - (4 + 8 + 16); // TODO use NUM_DIGITS and DIGIT_OFFSET
+	PORTD &= DIGIT_MASK;
 	PORTB = framebuf[cur_digit];
 	PORTD |= (1 << DIGIT_OFFSET) << cur_digit;
 }
@@ -28,7 +30,11 @@ int main(void) {
 
 	DDRB = 0xFF;
 	DDRD |= 4 + 8 + 16;
-	for (uint8_t digit = 0; digit < NUM_DIGITS; digit++) framebuf[digit] = 0xFF;
+
+	for (uint8_t digit = 0; digit < NUM_DIGITS; digit++) {
+		framebuf[digit] = 0xFF;
+		DIGIT_MASK &= ~(_BV(DIGIT_OFFSET) << digit);
+	}
 
 	TCCR1B = (1 << WGM12) | TIMER1_PRESCALE_1;
 	OCR1A = (uint16_t)5000;
